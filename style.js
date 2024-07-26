115ef9900a85a26131cda196b06b6829185fa961
Ilgiz Saburov <barysaburov@gmail.com>
2024-07-27 01:13:36 +0300

    fix to bags

--- style.js
+++ style.js
@@ -3,18 +3,27 @@ const twoMove = prompt('Enter the letter between a and h');
 const threeMove = prompt('Enter the number between 1 and 8');
 const fourMove = prompt('Enter the letter between a and h');
 
-const moveVariantOne = oneMove + fourMove;
-const moveVariantTwo = threeMove + twoMove;
-const moveOneCell = oneMove + twoMove;
-const moveTwoCell = threeMove + fourMove;
+if (
+    oneMove < 1 || oneMove > 8 
+    || threeMove < 1 || threeMove > 8 
+    || twoMove < 'a' || twoMove > 'h' 
+    || fourMove < 'a' || fourMove > 'h'
+) {
+    alert('Incorrect data entered. Please enter numbers between 1 and 8 and letters between a and h.');
+}
+    
+    const moveVariantOne = oneMove + fourMove;
+    const moveVariantTwo = threeMove + twoMove;
+    const moveOneCell = oneMove + twoMove;
+    const moveTwoCell = threeMove + fourMove;
 
 if (moveOneCell === moveTwoCell) {
-    alert('Rook stands still');
+    alert('Rook stands still!');
 } 
 
 if (oneMove === threeMove || twoMove === fourMove) {
-    alert(`The rook needs one move to reach the ${moveTwoCell}`);
+    alert(`The rook needs one move to reach the ${moveTwoCell} cell`);
 }   else {
-    alert(`The rook needs two move to reach square ${moveTwoCell}. 
+    alert(`The rook needs two move to reach the ${moveTwoCell} cell. 
     The first variant via the ${moveVariantOne} cell and the second variant via the ${moveVariantTwo} cell`); 
 }
